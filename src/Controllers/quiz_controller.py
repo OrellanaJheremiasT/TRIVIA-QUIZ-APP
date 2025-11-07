01d602b796921368e4cab6f3820f6c5f79c4421e
OrellanaJheremiasT <d4rkr4in.ttv@gmail.com>
2025-11-06 23:32:27 -0500

    select_difficulty

--- src/Controllers/quiz_controller.py
+++ src/Controllers/quiz_controller.py
@@ -1,16 +1,22 @@
 import random, time, html
 from models.api_model import get_questions
-from views.ui import clear, banner, progress_bar
+from views.ui import clear, banner, progress_bar, select_difficulty
 
 def run_quiz():
     clear()
     banner()
-    questions = get_questions(10)
+
+    difficulty = select_difficulty()
+    input("\nPress ENTER to start the quiz...")
+
+    questions = get_questions(amount=10, difficulty=difficulty)
     score = 0
+    total = len(questions)
+
     for i, q in enumerate(questions, 1):
         clear()
         banner()
-        progress_bar(i, len(questions))
+        progress_bar(i, total)
         question = html.unescape(q["question"])
         correct = html.unescape(q["correct_answer"])
         options = [html.unescape(x) for x in q["incorrect_answers"]] + [correct]
@@ -21,11 +27,16 @@ def run_quiz():
             print(f"{idx}) {op}")
 
         choice = input("\nYour answer (1-4): ")
-        if choice.isdigit() and options[int(choice)-1] == correct:
-            print("✅ Correct!\n")
-            score += 1
+        if choice.isdigit() and 1 <= int(choice) <= 4:
+            if options[int(choice)-1] == correct:
+                print(" Correct!\n")
+                score += 1
+            else:
+                print(f" Wrong. Correct: {correct}\n")
         else:
-            print(f"❌ Wrong. Correct: {correct}\n")
+            print(" Invalid choice.\n")
         time.sleep(1)
-    print(f"Final Score: {score}/{len(questions)}")
+
+    print(f" Final Score: {score}/{total}")
     input("\nPress ENTER to exit...")
+
--- src/Models/api_model.py
+++ src/Models/api_model.py
@@ -1,7 +1,13 @@
 import requests
 
-def get_questions(amount=10):
+import requests
+
+def get_questions(amount=10, difficulty="easy"):
     url = "https://opentdb.com/api.php"
-    params = {"amount": amount, "type": "multiple"}
+    params = {
+        "amount": amount,
+        "type": "multiple",
+        "difficulty": difficulty
+    }
     response = requests.get(url, params=params)
     return response.json()["results"]
